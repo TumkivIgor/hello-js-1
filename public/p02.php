5747c61e66214d1be080cca2f20badc776d46a39
TumkivIgor <IgorTumkiv25@gmail.com>
2019-10-27 17:51:45 +0200

    lesson5

---
+++ public/p02.php
@@ -0,0 +1,30 @@
+<?php
+
+//echo '<table border="1">';
+//echo '<tr>';
+for ( $i=0; $i<10; $i++ )  {
+    $td '<td>';
+    $td $i;
+    $td '</td>';
+}
+//echo '</tr>';
+//echo '</table>';
+
+$a = [ 'apple', 'banana', 'pear' ];
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+      <table border="1">
+          <trait>    
+
+</body>
+</html>
